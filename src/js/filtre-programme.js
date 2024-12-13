738cd7ddf942e02bd123557f01da923bf7ae8230
Lefrec <blackfox244@gmail.com>
2024-12-13 10:42:25 +0100

    nouveaux arrow-icon et fix carousel

---
+++ src/js/filtre-programme.js
@@ -0,0 +1,6 @@
+const btnFiltre = document.querySelector(".programme__btn");
+const seances = document.querySelectorAll(".programme__seance");
+
+btnFiltre.addEventListener("click", (event) => {
+    
+})
